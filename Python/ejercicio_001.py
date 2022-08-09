dd635b856245cff3f00f9553f4aa7a100a649f48
Hector <hector_ing2018@hotmail.com>
2022-08-09 19:21:08 -0300

    frist commit

--- Python/ejercicio_001.py
+++ Python/ejercicio_001.py
@@ -1,16 +1,3 @@
-"""
-##DATOS DE CONTACTO###
-    Nombre
-    Apellidos
-    Edad
-    Email
-    Teléfono
-    Casado (verdadero o falso)
-    Con Hijos (verdadero o falso)
-    Lista de amigos
-    Películas vistas (diccionario con clave y valor. El valor será el título de la película)
-    Una vez hayas creado todas las variables, muéstralas por consola 
-"""
 nombre = "Pepe"
 apellido = "Sanchez"
 edad = 23
@@ -19,4 +6,5 @@ casado = False
 hijos = False
 amigos = ["Juan","Pedro","Jose"]
 pelisVistas={'peli1':'Transformers', 'peli2':'Skyfall','peli3':'Capitana Marvel','peli4':'Capitana Marvel'}
+
 
