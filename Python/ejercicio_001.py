d08b83858cd39c337840f9d1c519b8d0f542f9a3
Hector <hector_ing2018@hotmail.com>
2022-08-09 20:05:03 -0300

    frist commit

--- Python/ejercicio_001.py
+++ Python/ejercicio_001.py
@@ -1,10 +1,31 @@
+from tabulate import tabulate
 nombre = "Pepe"
 apellido = "Sanchez"
 edad = 23
 email = "pepito@hotmail.com"
 casado = False
 hijos = False
-amigos = ["Juan","Pedro","Jose"]
-pelisVistas={'peli1':'Transformers', 'peli2':'Skyfall','peli3':'Capitana Marvel','peli4':'Capitana Marvel'}
+amigos = [["Juan"],["Pedro"],["Jose"]]
 
+pelisVistas={'peli1':['Transformers'], 
+            'peli2':['Skyfall'],
+            'peli3':['Capitana Marvel'],
+            'peli4':['Capitana Marvel']}
+
+print("+--------------------------+")
+print("|       CONTACTO           |")
+print("+--------------------------+")
+
+print(f"| NOMBRE: {nombre}\t \t   |")
+print(f"| APELLIDO: {apellido}\t   |")
+print(f"| EDAD: {edad}\t\t   |")
+print(f"| EMAIL: {email}|")
+print("+--------------------------+")
+print("----------------------------")
+print("\tPELICULAS VISTAS")
+print(tabulate(pelisVistas))
+print("+--------------------------+")
+print("\tLISTA DE AMIGOS")
+print("+--------------------------+")
+print(tabulate(amigos))
 
